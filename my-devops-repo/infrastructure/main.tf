81ec19fdb5179a2f46e7cb52ab7c85f0f50fe41d
WaelAA <wael.ibm@gmail.com>
2025-12-30 10:47:59 +0200

    Update main.tf

--- my-devops-repo/infrastructure/main.tf
+++ my-devops-repo/infrastructure/main.tf
@@ -22,19 +22,26 @@ resource "kubernetes_namespace_v1" "learning" {
 # 1. Define the Nginx Deployment
 resource "kubernetes_deployment_v1" "nginx" {
   metadata {
-    name   = "nginx-server"
-    labels = { app = "nginx" }
+    name = "nginx-server"
+    labels = {
+      app = "nginx"
+    }
   }
 
   spec {
-    replicas = 5
+    replicas = 2 # This ensures 2 copies are always running
+
     selector {
-      match_labels = { app = "nginx" }
+      match_labels = {
+        app = "nginx"
+      }
     }
 
     template {
       metadata {
-        labels = { app = "nginx" }
+        labels = {
+          app = "nginx"
+        }
       }
 
       spec {
@@ -42,19 +49,8 @@ resource "kubernetes_deployment_v1" "nginx" {
           image = "nginx:1.21.6"
           name  = "nginx-container"
 
-          # This is where the magic happens:
-          volume_mount {
-            name       = "config-volume"
-            mount_path = "/usr/share/nginx/html/index.html"
-            sub_path   = "WELCOME_MSG" # The key we defined in Ansible
-          }
-        }
-
-        # Define the volume source (The ConfigMap from Ansible)
-        volume {
-          name = "config-volume"
-          config_map {
-            name = "app-settings" # Must match the name in your Ansible playbook
+          port {
+            container_port = 80
           }
         }
       }
